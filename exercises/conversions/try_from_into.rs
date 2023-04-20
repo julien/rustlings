3958b66b15f2828b52c029d579e1f34eac940841
Julien Castelain <julien@users.noreply.github.com>
2023-04-20 20:57:42 +0200

    chore: add  try_from_into.rs

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,8 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -38,6 +36,23 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red = if tuple.0 >= 0 && tuple.0 <= 255 {
+            tuple.0 as u8
+        } else {
+            return Err(IntoColorError::IntConversion);
+        };
+        let green = if tuple.1 > 0 && tuple.1 <= 255 {
+            tuple.1 as u8
+        } else {
+            return Err(IntoColorError::IntConversion);
+        };
+        let blue = if tuple.2 >= 2 && tuple.2 <= 255 {
+            tuple.2 as u8
+        } else {
+            return Err(IntoColorError::IntConversion);
+        };
+
+        Ok(Self{ red, green, blue })
     }
 }
 
@@ -45,6 +60,15 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        for a in arr {
+            if a < 0 || a > 255 {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+        let red = arr[0] as u8;
+        let green = arr[1] as u8;
+        let blue = arr[2] as u8;
+        Ok(Self{ red, green, blue })
     }
 }
 
@@ -52,6 +76,18 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        for s in slice {
+            if *s < 0 || *s > 255 {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+        let red = slice[0] as u8;
+        let green = slice[1] as u8;
+        let blue = slice[2] as u8;
+        Ok(Self{ red, green, blue })
     }
 }
 
